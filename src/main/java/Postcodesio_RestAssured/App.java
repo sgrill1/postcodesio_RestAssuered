4985375dbdc3607940a340c9ae7eb614ea6a4dde
Sam Grill <sgrill@spartaglobal.com>
2018-11-08 09:46:42 +0000

    initial commit

---
+++ src/main/java/Postcodesio_RestAssured/App.java
@@ -0,0 +1,13 @@
+package Postcodesio_RestAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
